e9e2947d18c717428e17a11ee3ed8bc144bdd3a9
Jeremiah Chua <115858518+Ametrysinine@users.noreply.github.com>
2024-07-24 21:46:47 -0700

    Revert "Create basic MapContainer component"

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,11 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "./components/Navbar";
-import MapContainer from "./components/MapContainer"; // Note: CSS currently in MapContainer.jsx, use height 100% later
 
 const App = () => {
   return (
     <div className="w-full p-6">
       <Navbar />
       <Outlet />
-      <MapContainer /> 
     </div>
   );
 };
