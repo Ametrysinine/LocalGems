136291f9815dff615110d6f49f05d30b13ad7a5e
Jeremiah Chua <jeremiahjchua@hotmail.com>
2024-07-25 17:37:29 -0700

    Enable map movement

--- client/src/components/MapContainer.jsx
+++ client/src/components/MapContainer.jsx
@@ -2,7 +2,7 @@ import {APIProvider, Map} from '@vis.gl/react-google-maps';
 
 const MapContainer = () => (
   <APIProvider apiKey={import.meta.env.VITE_GOOGLE_MAPS_API_KEY}>
-    <Map zoom={10} center={{lat: 53.54992, lng: 10.00678}} />
+    <Map defaultCenter={{lat: 40.7, lng: -74}} defaultZoom={12}/>
   </APIProvider>
 );
 
