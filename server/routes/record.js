832be1b67a3950915e0ff5b9533c8859f2601b36
ChrisPytel <chris_pytel@hotmail.com>
2024-07-26 14:04:57 -0400

    initialize landing_page, login, and sign-up as routes

--- server/routes/record.js
+++ server/routes/record.js
@@ -1,15 +1,9 @@
 import express from "express";
+import db from "../db/connection.js";     // This will help us connect to the database
+import { ObjectId } from "mongodb";       // This help convert the id from string to ObjectId for the _id.
 
-// This will help us connect to the database
-import db from "../db/connection.js";
-
-// This help convert the id from string to ObjectId for the _id.
-import { ObjectId } from "mongodb";
-
-// router is an instance of the express router.
-// We use it to define our routes.
 // The router will be added as a middleware and will take control of requests starting with path /record.
-const router = express.Router();
+const router = express.Router();     // router is an instance of the express router. We use it to define our routes.
 
 // This section will help you get a list of all the records.
 router.get("/", async (req, res) => {
