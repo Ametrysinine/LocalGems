c608a7b3c801fc3c561a267023838d57ddf84cad
Cynthia <cynthia-lam@outlook.com>
2024-07-27 17:29:22 -0400

    Change router name from router_gems to router, change url to look for gems collection

--- server/routes/gems.js
+++ server/routes/gems.js
@@ -3,16 +3,16 @@ import db from "../db/connection.js";     // This will help us connect to the da
 import { ObjectId } from "mongodb";       // This help convert the id from string to ObjectId for the _id.
 
 // The router will be added as a middleware and will take control of requests starting with path /record.
-const router_gems = express.Router();     // router is an instance of the express router. We use it to define our routes.
+const router = express.Router();     // router is an instance of the express router. We use it to define our routes.
 
 
 // This section will help you get a list of all the records.
 router.get("/", async (req, res) => {
-  let collection = await db.collection("records");
+  let collection = await db.collection("gems");
   let results = await collection.find({}).toArray();
   res.send(results).status(200);
 });
 
-export default router_gems;
+export default router;
 
 // recommended to use curl to double check it before connecting to frontend
