9f68c0c63623a1b0f81c79f40b11d8f3c3de52a4
Cynthia <cynthia-lam@outlook.com>
2024-07-27 17:23:32 -0400

    Change table header and remove edit/delete functions for testing

--- client/src/components/GemList.jsx
+++ client/src/components/GemList.jsx
@@ -12,7 +12,7 @@ const GemListItem = (props) => (
     <td className="p-4 align-middle [&:has([role=checkbox])]:pr-0">
       {props.gem.city}
     </td>
-    <td className="p-4 align-middle [&:has([role=checkbox])]:pr-0">
+    {/* <td className="p-4 align-middle [&:has([role=checkbox])]:pr-0">
       <div className="flex gap-2">
         <Link
           className="inline-flex items-center justify-center whitespace-nowrap text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 border border-input bg-background hover:bg-slate-100 h-9 rounded-md px-3"
@@ -31,7 +31,7 @@ const GemListItem = (props) => (
           Delete
         </button>
       </div>
-    </td>
+    </td> */}
   </tr>
 );
 
@@ -54,14 +54,14 @@ export default function GemList() {
     return;
   }, [gems.length]);
 
-  // This method will delete a gem
-  async function deleteGem(id) {
-    await fetch(`http://localhost:5050/gems/${id}`, {
-      method: "DELETE",
-    });
-    const newGems = gems.filter((el) => el._id !== id);
-    setGems(newGems);
-  }
+  // // This method will delete a gem
+  // async function deleteGem(id) {
+  //   await fetch(`http://localhost:5050/gems/${id}`, {
+  //     method: "DELETE",
+  //   });
+  //   const newGems = gems.filter((el) => el._id !== id);
+  //   setGems(newGems);
+  // }
 
   // This method will map out the gems on the table
   function gemList() {
@@ -89,13 +89,10 @@ export default function GemList() {
                   Name
                 </th>
                 <th className="h-12 px-4 text-left align-middle font-medium text-muted-foreground [&:has([role=checkbox])]:pr-0">
-                  Position
+                  Description
                 </th>
                 <th className="h-12 px-4 text-left align-middle font-medium text-muted-foreground [&:has([role=checkbox])]:pr-0">
-                  Level
-                </th>
-                <th className="h-12 px-4 text-left align-middle font-medium text-muted-foreground [&:has([role=checkbox])]:pr-0">
-                  Action
+                  City
                 </th>
               </tr>
             </thead>
