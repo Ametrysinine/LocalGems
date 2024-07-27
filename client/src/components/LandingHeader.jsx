49ecfb840ba0b8cce62d98ecf1f8b1ba6623a1b4
ChrisPytel <chris_pytel@hotmail.com>
2024-07-26 20:48:19 -0400

    update components to test tailwind stylings

--- client/src/components/LandingHeader.jsx
+++ client/src/components/LandingHeader.jsx
@@ -4,9 +4,11 @@ import { useEffect, useState } from "react";
 const landingHeader = function() {
     return (
     <>
-      <h1 className="text-lg font-semibold p-4">Landing Page</h1>
+      <h1 className="text-lg font-semibold text-blue-600 italic text-2xl p-4 ">Landing Page</h1>
+      <p>this file is in /client/src/components/LandingHeader.jsx</p>
+
     </>
-  );  
+  );
   
 };
 
--- client/src/components/SignUp.jsx
+++ client/src/components/SignUp.jsx
@@ -4,7 +4,8 @@ import { useEffect, useState } from "react";
 const signUp = function() {
     return (
     <>
-      <h1 className="text-lg font-semibold p-4">Sign Up for LocalGems Here!</h1>
+      <h1 className="text-lg font-semibold text-orange-600 text-2xl italic p-4">Sign Up for LocalGems Here!</h1>
+      <p>this file is in /client/src/components/SignUp.jsx</p>
     </>
   );  
   
