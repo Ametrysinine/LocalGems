16c273727fff316f43f5c6c5384e90f2b6ba9758
Cynthia <cynthia-lam@outlook.com>
2024-07-28 16:20:25 -0400

    Add links to favourited, owned, unlocked gems

--- client/src/components/MyGems.jsx
+++ client/src/components/MyGems.jsx
@@ -4,6 +4,12 @@ const MyGems = (props) => {
   return(
     <>
     <h1 className="text-lg font-semibold text-blue-600 italic text-2xl p-4 ">The entire my-gem page component</h1>
+    <br/>
+    <a href="urlForOwnedQueryHere">My Gems (default)</a>
+    <br/>
+    <a href="urlForFavQueryHere">Favourited Gems</a>
+    <br/>
+    <a href="urlForUnlockedQueryHere">Unlocked Gems</a>
     <GemList/>
     </>
   );
