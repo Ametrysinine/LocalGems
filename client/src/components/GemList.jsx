5aedaa2a7197139cc694a807ccb1fa082877c52e
Cynthia <cynthia-lam@outlook.com>
2024-07-28 12:18:04 -0400

    Move GemListItem code from GemList to GemListItem

--- client/src/components/GemList.jsx
+++ client/src/components/GemList.jsx
@@ -1,39 +1,7 @@
 import { useEffect, useState } from "react";
 // import { Link } from "react-router-dom";
+import GemListItem from "./GemListItem";
 
-const GemListItem = (props) => (
-  <tr className="border-b transition-colors hover:bg-muted/50 data-[state=selected]:bg-muted">
-    <td className="p-4 align-middle [&:has([role=checkbox])]:pr-0">
-      {props.gem.name}
-    </td>
-    <td className="p-4 align-middle [&:has([role=checkbox])]:pr-0">
-      {props.gem.description}
-    </td>
-    <td className="p-4 align-middle [&:has([role=checkbox])]:pr-0">
-      {props.gem.city}
-    </td>
-    {/* <td className="p-4 align-middle [&:has([role=checkbox])]:pr-0">
-      <div className="flex gap-2">
-        <Link
-          className="inline-flex items-center justify-center whitespace-nowrap text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 border border-input bg-background hover:bg-slate-100 h-9 rounded-md px-3"
-          to={`/edit/${props.gem._id}`}
-        >
-          Edit
-        </Link>
-        <button
-          className="inline-flex items-center justify-center whitespace-nowrap text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 border border-input bg-background hover:bg-slate-100 hover:text-accent-foreground h-9 rounded-md px-3"
-          color="red"
-          type="button"
-          onClick={() => {
-            props.deleteGem(props.gem._id);
-          }}
-        >
-          Delete
-        </button>
-      </div>
-    </td> */}
-  </tr>
-);
 
 export default function GemList() {
   const [gems, setGems] = useState([]);
--- client/src/components/GemListItem.jsx
+++ client/src/components/GemListItem.jsx
@@ -1,11 +1,35 @@
-const GemListItem = function() {
-  return (
-  <>
-    <h1 className="text-lg font-semibold text-blue-600 italic text-2xl p-4 ">A component that displays a single gem within a gemlist</h1>
-    <p>this file is in /client/src/components/GemListItem.jsx</p>
-
-  </>
+const GemListItem = (props) => (
+  <tr className="border-b transition-colors hover:bg-muted/50 data-[state=selected]:bg-muted">
+    <td className="p-4 align-middle [&:has([role=checkbox])]:pr-0">
+      {props.gem.name}
+    </td>
+    <td className="p-4 align-middle [&:has([role=checkbox])]:pr-0">
+      {props.gem.description}
+    </td>
+    <td className="p-4 align-middle [&:has([role=checkbox])]:pr-0">
+      {props.gem.city}
+    </td>
+    {/* <td className="p-4 align-middle [&:has([role=checkbox])]:pr-0">
+      <div className="flex gap-2">
+        <Link
+          className="inline-flex items-center justify-center whitespace-nowrap text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 border border-input bg-background hover:bg-slate-100 h-9 rounded-md px-3"
+          to={`/edit/${props.gem._id}`}
+        >
+          Edit
+        </Link>
+        <button
+          className="inline-flex items-center justify-center whitespace-nowrap text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 border border-input bg-background hover:bg-slate-100 hover:text-accent-foreground h-9 rounded-md px-3"
+          color="red"
+          type="button"
+          onClick={() => {
+            props.deleteGem(props.gem._id);
+          }}
+        >
+          Delete
+        </button>
+      </div>
+    </td> */}
+  </tr>
 );
-};
 
 export default GemListItem;
