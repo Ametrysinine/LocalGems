dd0ecdc21deab557ef84afc57ba6c1fff984bba8
Cynthia <cynthia-lam@outlook.com>
2024-07-28 16:10:24 -0400

    Populate search form components

--- client/src/components/SearchForm.jsx
+++ client/src/components/SearchForm.jsx
@@ -1,9 +1,28 @@
 const SearchForm = function() {
   return (
-  <>
-    <h1 className="text-lg font-semibold text-blue-600 italic text-2xl p-4 ">The search form for /explore page</h1>
-  </>
-);
+    <>
+      <h1 className="text-lg font-semibold text-blue-600 italic text-2xl p-4 ">The search form for /explore page</h1>
+
+      <div className="searchFormContainer">
+        <form className="searchForm">
+          <label>Search by city:</label>
+          <input
+            className='inputBox'
+            placeholder="Toronto"
+          // onChange={(ev) => setEmail(ev.target.value)}
+          />
+          <br/>
+          <label>Search by keyword:</label>
+          <input
+            className='inputBox'
+            placeholder="Pizza"
+          // onChange={(ev) => setEmail(ev.target.value)}
+          />
+          <p>Dropdown for Gem type here</p>
+        </form>
+      </div>
+    </>
+  );
 };
 
 export default SearchForm;
