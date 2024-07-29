f0985e4ab323173accbe438195d265f38c950e92
Jeremiah Chua <jeremiahjchua@hotmail.com>
2024-07-29 16:55:39 -0700

    Implement sign up form

--- client/src/components/LoginForm.jsx
+++ client/src/components/LoginForm.jsx
@@ -5,13 +5,13 @@ const login = function() {
     return (
     <>
       <h1 className="text-lg font-semibold text-green-600 italic text-2xl p-4">Login to your LocalGems Account Here!</h1>
-      <p>this file is in /client/src/components/Login.jsx</p>
+      <p>this file is in /client/src/components/LoginForm.jsx</p>
     <form action="/login" method="post">
       <div class="container">
-        <label for="email">Email  </label>
+        <label for="email">Email </label>
         <input type="email" placeholder="Enter Email" id="email" name="email" required/> 
 
-        <label for="password">Password  </label>
+        <label for="password">Password </label>
         <input type="password" placeholder="Enter Password" id="password" name="password" required/>
         <button type="submit">Login</button>
       </div>
--- client/src/components/SignUpForm.jsx
+++ client/src/components/SignUpForm.jsx
@@ -5,7 +5,23 @@ const signUp = function() {
     return (
     <>
       <h1 className="text-lg font-semibold text-orange-600 text-2xl italic p-4">Sign Up for LocalGems Here!</h1>
-      <p>this file is in /client/src/components/SignUp.jsx</p>
+      <p>this file is in /client/src/components/SignUpForm.jsx</p>
+
+      <form action="sign-up/" method="post">
+        <container>
+          <label for="name">Name </label>
+          <input type="text" id="name" placeholder="Enter Name" name="name" required/>
+
+          <label for="email">Email </label>
+          <input type="email" id="email" placeholder="Enter Email" name="email" required/>
+
+          <label for="password">Password </label>
+          <input type="password" id="password" placeholder="Enter Password" name="password" required/>
+
+          <button type="submit">Sign Up</button>
+        </container>
+
+      </form>
     </>
   );  
   
