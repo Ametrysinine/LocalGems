cea86c6529e37fafcfa7ecd032987b5174c3cade
Cynthia <cynthia-lam@outlook.com>
2024-07-28 21:23:53 -0400

    Add state and useEffect in Explore.jsx to populate GemList search results

--- client/src/components/Explore.jsx
+++ client/src/components/Explore.jsx
@@ -3,12 +3,31 @@ import SearchForm from "./SearchForm";
 import GemList from "./GemList";
 
 const explore = function() {
+  const [gems, setGems] = useState([]);
+  const [filter, setFilter] = useState('');
+
+  // This method fetches the gems from the database.
+  useEffect(() => {
+    async function getGems() {
+      const response = await fetch(`http://localhost:5050/gems/${filter}`);
+      if (!response.ok) {
+        const message = `An error occurred: ${response.statusText}`;
+        console.error(message);
+        return;
+      }
+      const gems = await response.json();
+      setGems(gems);
+    }
+    getGems();
+    return;
+  }, [filter]);
+
     return (
     <>
       <h1 className="text-lg font-semibold text-blue-600 italic text-2xl p-4 ">The entire explore page component</h1>
       
       <SearchForm/>
-      <GemList/>
+      <GemList gems={gems}/>
 
     </>
   );
