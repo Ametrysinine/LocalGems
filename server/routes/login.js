ba7bb6d0980b510ee03bac13713d20f166a7d978
ChrisPytel <chris_pytel@hotmail.com>
2024-07-30 18:24:59 -0400

    update login.js route to sign JWT on succesful resolution of verifyLogin

--- server/routes/login.js
+++ server/routes/login.js
@@ -1,6 +1,14 @@
 import express from "express";
 import db from "../db/connection.js";     // This will help us connect to the database
 import { ObjectId } from "mongodb";       // This help convert the id from string to ObjectId for the _id.
+import * as dotenv from 'dotenv'
+dotenv.config()
+
+const SECRET = process.env.JWT_KEY
+
+
+import jwt from "jsonwebtoken";
+import verifyLogin from "../middleware/verifyLogin.js";
 
 // The router will be added as a middleware and will take control of requests starting with path /record.
 const router = express.Router();     // router is an instance of the express router. We use it to define our routes.
@@ -14,14 +22,29 @@ router.get("/", async (req, res) => {
 
 
 // Used for posting data to server from the login form
-  router.post("/", async (req, res) => {  
-    console.log(`\nEntered the POST login route with the following data:`, req.body);
-    const { emailField, passwordField} = req.body;
-    console.log(`Our email is: `, emailField);
-    console.log(`Our password is: `, passwordField);
+router.post("/", async (req, res) => {  
+  console.log(`\nEntered the POST login route with the following data:\n`, req.body);
+  const { emailField, passwordField} = req.body;  
+  
+  let users = await db.collection('users');
+  let databaseReturn = await users.find({email: `${emailField}`}).toArray(); 
 
+  const userData = verifyLogin(emailField, passwordField, databaseReturn); //returns userData obj after verifying login info against DB
+  console.log(`Our userData is: `, userData);
 
+  if (!userData){
+    console.error(`Bad login info, no JWT created`);
+    return res.status(406).json({message: 'Invalid credentials'})
+  }else{
 
+    jwt.sign(userData , SECRET, { expiresIn: '30d' }, function(error, token) {
+      if(error){
+        console.error('Error generating token');
+      }
+      console.log(`Our token is created! Containing:\n`, token);
+      return res.status(200).json({token}); //token successfully created and sent to client's localstorage
+    });
+  }
 });
 
 
