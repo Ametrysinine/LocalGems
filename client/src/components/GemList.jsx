1c5ccae5d55fe1a46fafaa9b46dbeba8d5a824ea
Cynthia <cynthia-lam@outlook.com>
2024-07-30 18:49:01 -0400

    Add condition rendering to display message if no gems in GemList

--- client/src/components/GemList.jsx
+++ client/src/components/GemList.jsx
@@ -5,17 +5,24 @@ import GemListItem from "./GemListItem";
 
 export default function GemList(props) {
 
+  function display() {
+    if (!props.gems.length) {
+      return "No Gems found.";
+    }
+    return props.gems.map(gem =>
+      <GemListItem
+        key={gem.id}
+        gem={gem} />
+    );
+  }
+
   return (
     <>
       <h3 className="text-lg font-semibold p-4">The GemList component</h3>
       <div className="border rounded-lg overflow-hidden">
         <div className="relative w-full overflow-auto">
           <ul className="gem-list">
-            {props.gems.map(gem =>
-              <GemListItem
-                key={gem.id}
-                gem={gem} />
-            )}
+            {display()}
             </ul>
         </div>
       </div>
