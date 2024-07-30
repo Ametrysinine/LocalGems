79c924ae0c08c1f527c2ae1849da18763dec0a69
ChrisPytel <chris_pytel@hotmail.com>
2024-07-30 18:43:41 -0400

    update verifyLogin.js to return object with userdata on success

--- server/middleware/verifyLogin.js
+++ server/middleware/verifyLogin.js
@@ -5,13 +5,18 @@ const verifyLogin = (emailField, passwordField, databaseReturn) => {
   user password input is: ${passwordField},
   and DB returned:\n`, databaseReturn);
 
-  if (!databaseReturn[0]){                        
-    console.error(`No content from DB, cannot validate`);   
+  if (!databaseReturn[0]){
+    console.error(`No content from DB, cannot validate`);
     return false;
   }
   else if (emailField === databaseReturn[0].email && passwordField === databaseReturn[0].password) {
-    console.log(`Success we have a match!`);  
-    return true;
+    console.log(`Success we have a match!`);
+    return { 
+      user_id: databaseReturn[0].user_id,
+      name: databaseReturn[0].name,
+      email: databaseReturn[0].name,
+      pfp: databaseReturn[0].pfp
+      }
   } 
   else{
     console.log(`Credentials are not a match`);
