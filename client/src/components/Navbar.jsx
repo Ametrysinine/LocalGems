0c22910624b6a18bec82ff03a1d7b49309678d47
Lubi Islam <lubaba.islam91@gmail.com>
2024-07-29 22:28:46 -0400

    Add div for navigation

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,27 +1,46 @@
 import { NavLink } from "react-router-dom";
-
+import "../styles/Navbar.scss";
 
 export default function Navbar() {  
   return (
-    <div>
-      <nav className="flex justify-between items-center mb-6">
-        <NavLink to="/">
-          <img alt="LocalGems logo" className="h-10 inline" src="https://i.imgur.com/Awp6faW.png"></img>
-        </NavLink>
+    <div className="nav-bar">
+      <div className="nav-bar-logo">
+        <img src="https://placehold.co/40x40"/>
+      </div>
 
+      <div className="nav-bar-link">
+        <a href="/explore">Explore</a>
+      </div> 
 
-        {/*Reminder to set up conditional rendering when signed in later */}
-
-        <div>          
-          <NavLink className="inline-flex items-center justify-center whitespace-nowrap text-md font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 border border-input bg-background hover:bg-slate-100 h-9 rounded-md px-3" to="/login">
-            Login
-          </NavLink>
-
-          <NavLink className="inline-flex items-center justify-center whitespace-nowrap text-md font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 border border-input bg-background hover:bg-slate-100 h-9 rounded-md px-3" to="/sign-up">
-            Sign up
-          </NavLink>
+      <div className="nav-bar-user">
+        <img src="https://placehold.co/40x40"/>
+        <div className="nav-bar-user-dropdown">
+          <a href="/my-gems">My Gems</a>
+          <a href="/my-gems">Settings</a>
+          <a href="/logout">Log out</a>
         </div>
-      </nav>
+      </div>
     </div>
+
+    // <div>
+    //   <nav className="flex justify-between items-center mb-6">
+    //     <NavLink to="/">
+    //       <img alt="LocalGems logo" className="h-10 inline" src="https://i.imgur.com/Awp6faW.png"></img>
+    //     </NavLink>
+
+
+    //     {/*Reminder to set up conditional rendering when signed in later */}
+
+    //     <div>          
+    //       <NavLink className="inline-flex items-center justify-center whitespace-nowrap text-md font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 border border-input bg-background hover:bg-slate-100 h-9 rounded-md px-3" to="/login">
+    //         Login
+    //       </NavLink>
+
+    //       <NavLink className="inline-flex items-center justify-center whitespace-nowrap text-md font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 border border-input bg-background hover:bg-slate-100 h-9 rounded-md px-3" to="/sign-up">
+    //         Sign up
+    //       </NavLink>
+    //     </div>
+    //   </nav>
+    // </div>
   );
 }
