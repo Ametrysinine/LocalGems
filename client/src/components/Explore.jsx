302f898d9878753a9a0fcea5328ea9f781138cbd
Cynthia <cynthia-lam@outlook.com>
2024-07-30 17:54:13 -0400

    Implement Explore to accept city and keyword and fetch filtered data from collection

--- client/src/components/Explore.jsx
+++ client/src/components/Explore.jsx
@@ -2,35 +2,41 @@ import { useEffect, useState } from "react";
 import SearchForm from "./SearchForm";
 import GemList from "./GemList";
 
-const explore = function() {
+const Explore = function() {
   const [gems, setGems] = useState([]);
   const [filter, setFilter] = useState('');
 
   // This method fetches the gems from the database.
   useEffect(() => {
     async function getGems() {
-      const response = await fetch(`http://localhost:5050/gems/${filter}`);
+      const response = await fetch(`http://localhost:5050/explore?${filter}`);
       if (!response.ok) {
         const message = `An error occurred: ${response.statusText}`;
         console.error(message);
         return;
       }
       const gems = await response.json();
+      console.log("gems: ", gems);
       setGems(gems);
     }
     getGems();
     return;
   }, [filter]);
 
-    return (
-    <>
-      <h1 className="text-lg font-semibold text-blue-600 italic text-2xl p-4 ">The entire explore page component</h1>
-      
-      <SearchForm/>
-      <GemList gems={gems}/>
+  const handleSearch = (city, keyword) => {
+    const query = [];
+    if (city) query.push(`city=${city}`);
+    if (keyword) query.push(`keyword=${keyword}`);
+    setFilter(query.join('&'));
+  };
 
+  return (
+    <>
+      <h1 className="text-lg font-semibold text-blue-600 italic text-2xl p-4">The entire explore page component</h1>
+      <SearchForm onSearch={handleSearch} />
+      <GemList gems={gems} />
     </>
   );
 };
 
-export default explore;
+export default Explore;
