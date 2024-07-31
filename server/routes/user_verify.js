fcbcd9dd743d82eee94e11121fdd1a452ab5a81f
ChrisPytel <chris_pytel@hotmail.com>
2024-07-30 23:43:53 -0400

    update route for user_verify.js to correctly return the req.userData from validateToken middleware

--- server/routes/user_verify.js
+++ server/routes/user_verify.js
@@ -1,23 +1,20 @@
 import express from "express";
 import db from "../db/connection.js";     // This will help us connect to the database
-import { ObjectId } from "mongodb";       // This help convert the id from string to ObjectId for the _id.
 import * as dotenv from 'dotenv'
 dotenv.config()
 
 const SECRET = process.env.JWT_KEY
-
 import jwt from "jsonwebtoken";
 import validateToken from "../middleware/validateToken.js";
 
 // The router will be added as a middleware and will take control of requests starting with path /record.
 const router = express.Router();     // router is an instance of the express router. We use it to define our routes.
 
-
-// Used for posting data to server from the login form
-router.get("/", async (req, res) => {  
-  console.log(`succesful GET to user-verify route`);
-
-  
+// Route for checking the validity of our JWT from client
+router.post("/", validateToken, (req, res) => {  
+  console.log(`succesful POST to user-verify route`);
+  console.log("user_verify.js has this for req.userData:\n", req.userData);
+  return res.status(200).json(req.userData);  
 });
 
 
