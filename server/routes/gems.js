d36c0c163f47726f59297a103f7ad07e3a07cb72
Cynthia <cynthia-lam@outlook.com>
2024-07-31 00:44:33 -0400

    Implement querying favourited_gems by gem id

--- server/routes/gems.js
+++ server/routes/gems.js
@@ -1,6 +1,6 @@
 import express from "express";
 import db from "../db/connection.js";     // This will help us connect to the database
-// import { ObjectId } from "mongodb";       // This help convert the id from string to ObjectId for the _id.
+import { ObjectId } from "mongodb";       // This help convert the id from string to ObjectId for the _id.
 
 // The router will be added as a middleware and will take control of requests starting with path /record.
 const router = express.Router();     // router is an instance of the express router. We use it to define our routes.
@@ -25,10 +25,22 @@ router.get("/unlocked_gems", async (req, res) => {
   res.json(results[0].unlocked_gems).status(200);
 });
 
-router.get("/liked_gems", async (req, res) => {
-  let users = await db.collection('users');
-  let results = await users.find({name: "Alex"}).toArray(); //after chris sets up cookies -> change to name of current user
-  res.json(results[0].liked_gems).status(200);
+router.get("/favourited_gems", async (req, res) => {
+  let usersCollection = await db.collection('users');
+  let currentUser = await usersCollection.find({name: "Alex"}).toArray(); //after chris sets up cookies -> change to name of current user
+  console.log("currentuser: ", currentUser);
+
+  // list of the objectIDs
+  let favouritedIds = currentUser[0].favourited_gems; 
+  console.log("fav ids type:", typeof(favouritedIds));
+  // if they come out as strings, will need the following line
+  // let favouritedObjectIds = favouritedIds.map(id => ObjectId(id));
+
+  let gemsCollection = await db.collection('gems');
+  // this will be the list of all gems corresponding to the gem objectIDs
+  let favouritedGems = await gemsCollection.find({ _id: { $in: favouritedIds } }).toArray(); 
+  
+  res.json(favouritedGems).status(200);
 });
 
 export default router;
