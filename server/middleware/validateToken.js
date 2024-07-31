0bdf4fce4dc206659e01c6cd123a82fb2dcd4ca2
ChrisPytel <chris_pytel@hotmail.com>
2024-07-30 23:45:25 -0400

    update validateToken.js middleware to send back correct data on decoding JWT

--- server/middleware/validateToken.js
+++ server/middleware/validateToken.js
@@ -3,6 +3,25 @@ import * as dotenv from 'dotenv'
 dotenv.config()
 
 const SECRET = process.env.JWT_KEY
-const KEY_STRING = "Testkey123"
 
-console.log(`Our SECRET is: `, SECRET);
+const validateToken = (req, res, next) => {
+  const {user_token} = req.body;
+
+  console.log(`Our req.body is: `, req.body);
+  console.log(`Our user_token is: `, user_token);
+
+  jwt.verify(user_token, SECRET, (error, decoded) => {
+      if(error){
+          return res.status(401).json({message: 'Unauthorized'});
+      }
+      req.userData = {
+          user_id: decoded.user_id,
+          name: decoded.name,
+          email: decoded.email,
+          pfp: decoded.pfp
+      }
+      next();
+  });
+}
+
+export default validateToken;
