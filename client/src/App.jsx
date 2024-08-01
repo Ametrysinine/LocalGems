869fda91763cbf472a5895efc6aa6013a14f021f
Lubi Islam <lubaba.islam91@gmail.com>
2024-08-01 01:33:34 -0400

    Remove extraneous page styling

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,11 +6,7 @@ const App = () => {
   return (
     <div className="w-full p-6">
       <Navbar />
-      <article className="page-body">
-        <section className="page-body-content">
-          <Outlet />
-        </section>
-      </article>
+      <Outlet />
       <MapContainer /> {/*Note: Current height is height of container (i.e. 0 px)*/}
     </div>
   );
