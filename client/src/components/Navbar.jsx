e0c2a378f0043643baa329a127840bc507c20443
ChrisPytel <chris_pytel@hotmail.com>
2024-08-01 17:43:09 -0400

    update Navbar.jsx to use useContext for DB calls

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,57 +1,53 @@
 import { NavLink } from "react-router-dom";
 import { useEffect } from "react";
 import "../styles/Navbar.scss";
+import { useToken } from "../contexts/TokenContext";
 
-import useValidateToken from "../hooks/useValidateToken";
+// import useValidateToken from "../hooks/useValidateToken";
 
 export default function Navbar() {  
 
-  const { user, error } = useValidateToken(localStorage.getItem(`token`)); 
+  const { user, error, validateToken } = useToken(); 
+
+  useEffect(() => {
+    if (!user) {
+      validateToken(localStorage.getItem(`token`))
+    }
+  }, [user])
  
 
   useEffect (() => {
     const checkToken = async () => { //run initially on page load
-    if (!localStorage.getItem(`token`)) {
-      console.log(`token doesnt exist`);
-    }
-    else {
-      console.log(`----------------- token exists---------------------`); 
-      // const data = await validateToken(localStorage.getItem(`token`));    
-      // console.log(data)
-      if (user) {
-        console.log(`Our decrypted data associated under localStorage is: `, user);
-        try {  
-          console.log("In Try")     
-  
-          let response = await fetch(`http://localhost:5050/gems/posted_gems?user=${user.name}`, {
-              method: "GET",
-              headers: {
-                "Content-Type": "application/json",
-              },              
-            });    
-          if (!response.ok) {
-            throw new Error(`HTTP error! status: ${response.status}`);
-          }
-          else{
-            console.log(`got something back from server!!!!!!!`);
-          }
-  
-        } catch (error) {
-          console.error(`Error validating token: ${error}`);
-        }
+      if (!localStorage.getItem(`token`)) {
+        console.log(`token doesnt exist`);
       }
-       
-    }       
-  } 
-  checkToken();
- } ,[user])
-
-
-
-
-
-  
-
+      else {
+        console.log(`----------------- token exists---------------------`); 
+        if (user) {
+          console.log(`Our decrypted data associated under localStorage is: `, user);
+          try {  
+            console.log("In Try for Navbar.JSX")     
+    
+            let response = await fetch(`http://localhost:5050/gems/posted_gems?user=${user.name}`, {
+                method: "GET",
+                headers: {
+                  "Content-Type": "application/json",
+                },              
+              });    
+            if (!response.ok) {
+              throw new Error(`HTTP error! status: ${response.status}`);
+            }
+            else{
+              console.log(`got something back from server!!!!!!!`);
+            }    
+          } catch (error) {
+            console.error(`Error validating token: ${error}`);
+          }
+        }       
+      }       
+    } 
+    checkToken();
+  } ,[user])
 
 
   return (
