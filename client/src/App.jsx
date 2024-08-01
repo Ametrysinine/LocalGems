762e04b511778c88db13859d18e9f900e2c39f81
ChrisPytel <chris_pytel@hotmail.com>
2024-08-01 16:47:17 -0400

    refactor App.jsx to be wrapped in TokenProvider component at its highest level in react router

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,17 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import MapContainer from "./components/MapContainer"; 
+import TokenProvider from "./contexts/TokenContext";
 
 const App = () => {
   return (
+    <TokenProvider>
     <div className="w-full p-6">
       <Navbar />
       <Outlet />
       <MapContainer /> {/*Note: Current height is height of container (i.e. 0 px)*/}
     </div>
+    </TokenProvider>
   );
 };
 export default App
