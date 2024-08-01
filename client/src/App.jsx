6f291cccdc73ac9dcf8d757aad599139ed6d4e62
Lubi Islam <lubaba.islam91@gmail.com>
2024-08-01 01:12:09 -0400

    Add article and section around <Outlet /> to implement global page styling

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,7 +6,11 @@ const App = () => {
   return (
     <div className="w-full p-6">
       <Navbar />
-      <Outlet />
+      <article className="page-body">
+        <section className="page-body-content">
+          <Outlet />
+        </section>
+      </article>
       <MapContainer /> {/*Note: Current height is height of container (i.e. 0 px)*/}
     </div>
   );
