63845305318f36b7c0285ea9ccc5c5d432cc440c
Lubi Islam <lubaba.islam91@gmail.com>
2024-08-01 01:15:21 -0400

    Add divs to implement CSS grid

--- client/src/components/SearchForm.jsx
+++ client/src/components/SearchForm.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import "../styles/SearchForm.scss";
 
 const SearchForm = (props) => {
   const [city, setCity] = useState('');
@@ -9,40 +10,46 @@ const SearchForm = (props) => {
 
   return (
     <>
-      <h1 className="text-lg font-semibold text-blue-600 italic text-2xl p-4">The search form for /explore page</h1>
+      {/* The search form for /explore page */}
 
       <div className="searchFormContainer">
-        <label>Search by city:</label>
-        <input
-          className='inputBox'
-          placeholder="e.g. Toronto"
-          value={city}
-          onChange={(ev) => setCity(ev.target.value)}
-        />
+        <div>
+          <label>Search by city:</label>
+          <input
+            className='inputBox'
+            placeholder="e.g. Toronto"
+            value={city}
+            onChange={(ev) => setCity(ev.target.value)}
+          />
+        </div>
 
-        <label>Search by keyword:</label>
-        <input
-          className='inputBox'
-          placeholder="e.g. pizza"
-          value={keyword}
-          onChange={(ev) => setKeyword(ev.target.value)}
-        />
+        <div>
+          <label>Search by keyword:</label>
+          <input
+            className='inputBox'
+            placeholder="e.g. pizza"
+            value={keyword}
+            onChange={(ev) => setKeyword(ev.target.value)}
+          />
+        </div>
 
-        <label>Search by type:</label>
-        <select
-          className='inputDropdown'
-          value={type}
-          onChange={(e) => setType(e.target.value)}>
-          <option value="">Select a type</option>
-          <option value="food">Food</option>
-          <option value="outdoors">Outdoors</option>
-          <option value="shopping">Shopping</option>
-          <option value="entertainment">Entertainment</option>
-          <option value="nightlife">Nightlife</option>
-          <option value="services">Services</option>
-        </select>
+        <div>
+          <label>Search by type:</label>
+          <select
+            className='inputDropdown'
+            value={type}
+            onChange={(e) => setType(e.target.value)}>
+            <option value="">Select a type</option>
+            <option value="food">Food</option>
+            <option value="outdoors">Outdoors</option>
+            <option value="shopping">Shopping</option>
+            <option value="entertainment">Entertainment</option>
+            <option value="nightlife">Nightlife</option>
+            <option value="services">Services</option>
+          </select>
 
-        <button onClick={() => onSearch(city, keyword, type)}>Submit</button>
+          <button onClick={() => onSearch(city, keyword, type)}>Submit</button>
+        </div>
       </div>
     </>
   );
