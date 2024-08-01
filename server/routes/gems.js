375bc40ea99464895ac7d3946e053f13a7f22a98
Cynthia <cynthia-lam@outlook.com>
2024-07-31 22:08:09 -0400

    Update unlocked_gems query to match changes in db - unlocked_gems now an array of ObjectId

--- server/routes/gems.js
+++ server/routes/gems.js
@@ -14,35 +14,42 @@ router.get("/", async (req, res) => {
 });
 
 router.get("/posted_gems", async (req, res) => {
+  //query users db to find array of posted_gems
   const users = await db.collection('users');
   const currentUser = await users.find({name: "Alex"}).toArray(); //after chris sets up cookies -> change to name of current user
   const ownedIds = currentUser[0].posted_gems; 
 
-  const gemsCollection = await db.collection('gems');
-  // this will be the list of all gems corresponding to the gem objectIDs
-  const ownedGems = await gemsCollection.find({ _id: { $in: ownedIds } }).toArray(); 
+  // query gems collection to find gems that match the objectIds 
+  const gems = await db.collection('gems');
+  const ownedGems = await gems.find({ _id: { $in: ownedIds } }).toArray(); 
 
-  res.json(ownedGems).status(200); // change this to searching by ID (like favourited_gems)
+  res.json(ownedGems).status(200);
 });
 
 router.get("/unlocked_gems", async (req, res) => {
+  //query users db to find array of posted_gems
   const users = await db.collection('users');
-  const results = await users.find({name: "Alex"}).toArray(); //after chris sets up cookies -> change to name of current user
-  res.json(results[0].unlocked_gems).status(200);
+  const currentUser = await users.find({name: "Alex"}).toArray(); //after chris sets up cookies -> change to name of current user
+  const unlockedIds = currentUser[0].unlocked_gems; 
+
+  // query gems collection to find gems that match the objectIds 
+  const gems = await db.collection('gems');
+  const unlockedGems = await gems.find({ _id: { $in: unlockedIds } }).toArray(); 
+  
+  res.json(unlockedGems).status(200);
 });
 
 router.get("/favourited_gems", async (req, res) => {
+  //query users db to find array of posted_gems
   const usersCollection = await db.collection('users');
   const currentUser = await usersCollection.find({name: "Alex"}).toArray(); //after chris sets up cookies -> change to name of current user
-
-  // list of the objectIDs
   const favouritedIds = currentUser[0].favourited_gems; 
   // if they come out as strings, will need the following line
   // const favouritedObjectIds = favouritedIds.map(id => ObjectId(id));
 
-  const gemsCollection = await db.collection('gems');
-  // this will be the list of all gems corresponding to the gem objectIDs
-  const favouritedGems = await gemsCollection.find({ _id: { $in: favouritedIds } }).toArray(); 
+  // query gems collection to find gems that match the objectIds 
+  const gems = await db.collection('gems');
+  const favouritedGems = await gems.find({ _id: { $in: favouritedIds } }).toArray(); 
   
   res.json(favouritedGems).status(200);
 });
