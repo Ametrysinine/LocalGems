512869753474670711aa43aa9f6eab5e2dff6391
Cynthia <cynthia-lam@outlook.com>
2024-08-01 22:22:45 -0400

    Add POST route for /gems/create

--- server/routes/gems.js
+++ server/routes/gems.js
@@ -1,5 +1,6 @@
 import express from "express";
 import db from "../db/connection.js";     // This will help us connect to the database
+import { ObjectId } from "mongodb";
 
 // The router will be added as a middleware and will take control of requests starting with path /record.
 const router = express.Router();     // router is an instance of the express router. We use it to define our routes.
@@ -20,7 +21,7 @@ router.get("/:filter", async (req, res) => {
 
   const users = await db.collection('users');
   const currentUser = await users.find({name: queryToSendToDBSuperImportant}).toArray();
-  console.log("currentUser: ", currentUser);
+  // console.log("currentUser: ", currentUser);
 
   let filteredGemIds = [];
     if (filter === 'posted_gems') {
@@ -30,12 +31,52 @@ router.get("/:filter", async (req, res) => {
     } else if (filter === 'unlocked_gems') {
       filteredGemIds = currentUser[0].unlocked_gems;
     }
-    console.log("filter: ", filter, "filtered gem ids: ", filteredGemIds);
+    // console.log("filter: ", filter, "filtered gem ids: ", filteredGemIds);
 
   const gems = await db.collection('gems');
   const filteredGems = await gems.find({ _id: { $in: filteredGemIds } }).toArray(); 
   
   res.json(filteredGems).status(200);
+});
+
+// This will create a new Gem in the db
+router.post('/create', async (req, res) => {
+  console.log("-------POST REQUEST TO /GEMS/CREATE-------");
+  const gems = await db.collection("gems");
+  
+  const { name, description, city, address, latitude, longitude, images, type } = req.body;
+  const userId = req.query.user_id;
+  console.log("userid: ", userId);
+  
+
+  const newGem = {
+    _id: new ObjectId(),
+    name,
+    description,
+    city,
+    location: {
+      address,
+      latitude,
+      longitude
+    },
+    images: images.split(',').map((image) => image.trim()),
+    type,
+    createdAt: new Date(),
+    total_score: 0,
+    owner_id: userId
+  };
+  console.log("newGem is: ", newGem);
+  
+
+  try {
+    const result = await gems.insertOne(newGem);
+    res.json(result).status(200);
+    console.log("restult: ", result);
+    
+  } catch (error) {
+    console.error('Failed to create gem:', error);
+    res.status(500).json({ message: 'Internal server error' });
+  }
 });
 
 export default router;
