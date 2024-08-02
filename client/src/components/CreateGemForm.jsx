05a7addaab3b300fd8a7e624961ac14377abd91d
Cynthia <cynthia-lam@outlook.com>
2024-08-01 20:25:20 -0400

    Create CreateGemForm.jsx

---
+++ client/src/components/CreateGemForm.jsx
@@ -0,0 +1,14 @@
+import { useEffect, useState } from "react";
+
+
+const CreateGemForm = function() {
+    return (
+    <>
+      <h1 className="text-lg font-semibold text-blue-600 italic text-2xl p-4 ">CreateGemForm component</h1>
+      <p>this file is in /client/src/components/CreateGemForm.jsx</p>
+
+    </>
+  );
+};
+
+export default CreateGemForm;
