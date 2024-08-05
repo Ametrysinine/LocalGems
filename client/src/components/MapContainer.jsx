0313ea4b95db4ccb9684589a2fb8d7a0ac0a6d7c
Jeremiah Chua <jeremiahjchua@hotmail.com>
2024-08-04 20:27:20 -0700

    Style modal

--- client/src/components/MapContainer.jsx
+++ client/src/components/MapContainer.jsx
@@ -2,14 +2,14 @@ import {APIProvider, Map} from '@vis.gl/react-google-maps';
 import {MapGem} from './MapGem';
 
 const MapContainer = (props) => {
-  const DisplayedGems = props.gems?.map(gem => <MapGem key={gem._id} location={gem.location} description={gem.description} type={gem.type} name={gem.name}>
+  const DisplayedGems = props.gems?.map(gem => <MapGem key={gem._id} location={gem.location} description={gem.description} type={gem.type} name={gem.name} images={gem.images}>
     </MapGem>)
   const firstGemPosition = props.gems[0].location;
   const mapStartingPosition = {lat: parseFloat(firstGemPosition.latitude.$numberDecimal), lng: parseFloat(firstGemPosition.longitude.$numberDecimal)}
 
   return (
     <APIProvider apiKey={import.meta.env.VITE_GOOGLE_MAPS_API_KEY}>
-      <Map defaultCenter={mapStartingPosition} defaultZoom={12} mapId='2e8b760e6bb005ff' libraries={['marker'] }>
+      <Map defaultCenter={mapStartingPosition} defaultZoom={12} mapId='2e8b760e6bb005ff' libraries={['marker']}  >
         {DisplayedGems}
       </Map>
     </APIProvider>
--- client/src/components/Modal.jsx
+++ client/src/components/Modal.jsx
@@ -10,16 +10,15 @@ export default function Component(props) {
   return (
     <>
       <Button onClick={() => setOpenModal(true)}>View</Button>
-      <Modal dismissible show={openModal} onClose={() => setOpenModal(false)}>
-        <Modal.Header>{props.name}</Modal.Header>
-        <Modal.Body>
-          <div className="w-[400px] h-[400px]">
-          <MapContainer gems={[props.gem]}/>
-          </div>
-        </Modal.Body>
-        <Modal.Footer>
-          <Button onClick={() => setOpenModal(false)}>Close</Button>
-        </Modal.Footer>
+      <Modal dismissible show={openModal} onClose={() => setOpenModal(false)} className="w-auto h-full">
+        <Button className="flex flex-row basis-1/4" onClick={() => setOpenModal(false)}>Close</Button>
+        <div className="h-[600px]"> 
+          <Modal.Body className="size-full">
+            <div className="size-full">
+              <MapContainer className="size-full" gems={[props.gem]}/>
+            </div>
+          </Modal.Body>
+        </div>
       </Modal>
     </>
   );
