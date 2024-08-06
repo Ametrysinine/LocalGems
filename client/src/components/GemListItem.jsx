676bfd69972d13c5cf90b637166910dcad280b0e
Cynthia <cynthia-lam@outlook.com>
2024-08-05 20:24:00 -0400

    Add thumpsup icon to GemListItem and begin splitting into divs

--- client/src/components/GemListItem.jsx
+++ client/src/components/GemListItem.jsx
@@ -23,35 +23,32 @@ const GemListItem = (props) => {
     }
   };
 
-  return(
-    
-  <tr className="border-b transition-colors hover:bg-muted/50 data-[state=selected]:bg-muted">
-    <td className="p-4 align-middle [&:has([role=checkbox])]:pr-0">
-      {props.gem.name}
-    </td>
-    <td className="p-4 align-middle [&:has([role=checkbox])]:pr-0">
+  return (
+
+    <div>
       {props.gem.description}
-    </td>
-    <td className="p-4 align-middle [&:has([role=checkbox])]:pr-0">
       {props.gem.city}
-    </td>
-    <td className="p-4 align-middle [&:has([role=checkbox])]:pr-0">
       {props.gem.images}
-    </td>
-    <td className="p-4 align-middle [&:has([role=checkbox])]:pr-0">
-      {props.gem.total_score}
-    </td>
-    <td className="p-4 align-middle [&:has([role=checkbox])]:pr-0">
       {dateConversion(props.gem.date_shared)}
-    </td>
-    <td className="p-4 align-middle [&:has([role=checkbox])]:pr-0">
+      <div className="gem-list__item_bottom-row">
+
+        <img src="thumbsup.png" alt="thumbs up" className="thumbs-image"/>
+        {props.gem.total_score}
         <button>Reveal {gemImage()}</button>
-      </td>
+      </div>
 
-    <Modal gem={props.gem}/>
+      <Modal gem={props.gem} />
 
 
-    {/* <td className="p-4 align-middle [&:has([role=checkbox])]:pr-0">
+
+    </div>
+  )
+};
+
+export default GemListItem;
+
+{/* 
+      <td className="p-4 align-middle [&:has([role=checkbox])]:pr-0">
       <div className="flex gap-2">
         <Link
           className="inline-flex items-center justify-center whitespace-nowrap text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 border border-input bg-background hover:bg-slate-100 h-9 rounded-md px-3"
@@ -71,8 +68,3 @@ const GemListItem = (props) => {
         </button>
       </div>
     </td> */}
-    </tr>
-  );
-};
-
-export default GemListItem;
