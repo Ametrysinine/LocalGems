3c973d67436971e3aa509f793ab726f5aa9dd9cc
ChrisPytel <chris_pytel@hotmail.com>
2024-08-05 22:56:45 -0400

    fix typos in NavGemCounter.jsx

--- client/src/components/NavGemCounter.jsx
+++ client/src/components/NavGemCounter.jsx
@@ -9,42 +9,42 @@ export default function NavGemCounter(props) {
         <img src="assets/flaticons/gem_ruby.png" alt="Ruby - Food" />
         <p>Rubies: <b>3</b></p>
         <div className="nav-currency-content">
-          <p>Create Entries in the <b>Food</b> category to earn Rubies</p>
+          <p>Create entries in the <b>Food</b> category to earn Rubies</p>
         </div>
       </div>
       <div className="nav-currency">
         <img src="assets/flaticons/gem_sapphire.png" alt="Sapphire - Entertainment"/>
         <p>Sapphires: <b>0</b></p>
         <div className="nav-currency-content">
-          <p>Create Entries in the <b>Entertainment</b> category to earn Emeralds</p>
+          <p>Create entries in the <b>Entertainment</b> category to earn Sapphires</p>
         </div>
       </div>
       <div className="nav-currency">
         <img src="assets/flaticons/gem_emerald.png" alt="Emerald - Outdoor Activity"/>
         <p>Emeralds: <b>1</b></p>
         <div className="nav-currency-content">
-          <p>Create Entries in the <b>Nature</b> category to earn Emeralds</p>
+          <p>Create entries in the <b>Nature</b> category to earn Emeralds</p>
         </div>
       </div>
       <div className="nav-currency">
         <img src="assets/flaticons/gem_topaz.png" alt="Topaz - Shopping"/>
         <p>Topazs: <b>2</b></p>
         <div className="nav-currency-content">
-          <p>Create Entries in the <b>Shopping</b> category to earn Topazs</p>
+          <p>Create entries in the <b>Shopping</b> category to earn Topazs</p>
         </div>
       </div>
       <div className="nav-currency">
         <img src="assets/flaticons/gem_amethyst.png" alt="Amethyst - Nightlife"/>
         <p>Amethysts: <b>1</b></p>
         <div className="nav-currency-content">
-          <p>Create Entries in the <b>Nightlife</b> category to earn Amethysts</p>
+          <p>Create entries in the <b>Nightlife</b> category to earn Amethysts</p>
         </div>
       </div>
       <div className="nav-currency">
         <img src="assets/flaticons/gem_citrine.png" alt="Citrine - Services"/>
         <p>Citrines: <b>1</b></p>
         <div className="nav-currency-content">
-          <p>Create Entries in the <b>Services</b> category to earn Citrines</p>
+          <p>Create entries in the <b>Services</b> category to earn Citrines</p>
         </div>
       </div>
     </div> 
