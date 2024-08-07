4e4142e4b3b7755998635f5fd57bddc8b45d366a
ChrisPytel <chris_pytel@hotmail.com>
2024-08-07 15:54:53 -0400

    update verifyLogin.js to return first city in locations array from DB

--- server/middleware/verifyLogin.js
+++ server/middleware/verifyLogin.js
@@ -11,11 +11,13 @@ const verifyLogin = (emailField, passwordField, databaseReturn) => {
   }
   else if (emailField === databaseReturn[0].email && passwordField === databaseReturn[0].password) {
     console.log(`Success we have a match!`);
+    console.log(`CORRESPONDING CITY IS: `, databaseReturn[0].locations[0].city_name);
     return { 
       user_id: databaseReturn[0].user_id,
       name: databaseReturn[0].name,
       email: databaseReturn[0].email,
-      pfp: databaseReturn[0].pfp
+      pfp: databaseReturn[0].pfp,
+      city_name: databaseReturn[0].locations[0].city_name
       }
   } 
   else{
