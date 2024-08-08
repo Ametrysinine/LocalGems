d5a20f5b9cdc7dff6146e8dc0bfc28f95e5098f6
Cynthia <cynthia-lam@outlook.com>
2024-08-07 21:59:21 -0400

    Take in new form params and user.name and user.user_id, createRandId

--- server/routes/gems.js
+++ server/routes/gems.js
@@ -11,17 +11,17 @@ const router = express.Router();     // router is an instance of the express rou
 // Favourite a gem----------------------------------------------------------------------------------------
 router.get("/favourite/:user_id/:gem_id", async (req, res) => {
   console.log("-----correct path to favourite!-----");
-  
+
   const collection = await db.collection("users");
   const userId = req.params.user_id; // aLZ3b1
-  const gemObj = {"$oid": req.params.gem_id} // $oid signifies object ID
-  res.status(200).send(await collection.updateOne({user_id: userId}, {$addToSet: {favourited_gems: gemObj}}));
+  const gemObj = { "$oid": req.params.gem_id } // $oid signifies object ID
+  res.status(200).send(await collection.updateOne({ user_id: userId }, { $addToSet: { favourited_gems: gemObj } }));
 });
 
 // // Add a gem to unlocked_gems----------------------------------------------------------------------------------------
 // router.get("/unlock/:user_id/:gem_id", async (req, res) => {
 //   console.log("-----correct path to unlock!-----");
-  
+
 //   const collection = await db.collection("users");
 //   const userId = req.params.user_id;
 //   const gemObj = {"$oid": req.params.gem_id} // $oid signifies object ID
@@ -38,10 +38,10 @@ router.get("/", async (req, res) => {
 // Retrieve gems for posted_gems----------------------------------------------------------------------------------------
 router.get("/posted_gems", async (req, res) => {
   const userIDToSendToDBSuperImportant = req.query.user;
-  
+
   const gems = await db.collection('gems');
-  const filteredGems = await gems.find({ owner_id: userIDToSendToDBSuperImportant }).sort({ date_shared: -1 }).toArray(); 
-  
+  const filteredGems = await gems.find({ owner_id: userIDToSendToDBSuperImportant }).sort({ date_shared: -1 }).toArray();
+
   res.json(filteredGems).status(200);
 });
 
@@ -50,28 +50,28 @@ router.get("/posted_gems", async (req, res) => {
 // Retrieve gems based on user_id for favourited and unlocked----------------------------------------------------------------
 router.get("/:filter", async (req, res) => {
   console.log("Entered /:filter");
-  
+
   const filter = req.params.filter;
   const userIDToSendToDBSuperImportant = req.query.user;
   console.log(`Our userIDToSendToDBSuperImportant is: `, userIDToSendToDBSuperImportant);
 
   const users = await db.collection('users');
-  const currentUser = await users.find({user_id: userIDToSendToDBSuperImportant}).toArray();
+  const currentUser = await users.find({ user_id: userIDToSendToDBSuperImportant }).toArray();
   console.log("currentUser: ", currentUser);
 
   let filteredGemIds = [];
-    if (filter === 'favourited_gems') {
-      filteredGemIds = currentUser[0].favourited_gems;
-    } else if (filter === 'unlocked_gems') {
-      filteredGemIds = currentUser[0].unlocked_gems;
-    }
-    console.log("filter: ", filter, "filtered gem ids (strings now): ", filteredGemIds);
+  if (filter === 'favourited_gems') {
+    filteredGemIds = currentUser[0].favourited_gems;
+  } else if (filter === 'unlocked_gems') {
+    filteredGemIds = currentUser[0].unlocked_gems;
+  }
+  console.log("filter: ", filter, "filtered gem ids (strings now): ", filteredGemIds);
 
   const gems = await db.collection('gems');
-  const filteredGems = await gems.find({ gem_id: { $in: filteredGemIds } }).toArray(); 
+  const filteredGems = await gems.find({ gem_id: { $in: filteredGemIds } }).toArray();
   console.log("filteredGems: ", filteredGems);
-  
-  
+
+
   res.json(filteredGems).status(200);
 });
 
@@ -79,10 +79,22 @@ router.get("/:filter", async (req, res) => {
 
 // This will create a new Gem in the db--------------------------------------------------------------------------------
 router.post('/create', async (req, res) => {
+  function createRandId() {
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    const idLength = 6;
+    let randId = '';
+    for (let i = 0; i < idLength; i++) {
+      const randomIndex = Math.floor(Math.random() * characters.length);
+      randId += characters[randomIndex];
+    }
+    return randId;
+  }
+
   const gems = await db.collection("gems");
 
-  const { name, description, city, address, latitude, longitude, images, type } = req.body;
-  const userId = req.query.user_id;
+  const { name, description, city, address, latitude, longitude, images, tags, whats_great_about_it, type } = req.body;
+  const userId = req.query.userId;
+  const userName = req.query.username;
 
   const newGem = {
     _id: new ObjectId(),
@@ -94,17 +106,24 @@ router.post('/create', async (req, res) => {
       latitude,
       longitude
     },
-    images: images.split(',').map((image) => image.trim()),
-    type,
     date_shared: new Date(),
+    images: images,
     total_score: 0,
-    owner_id: userId
+    owner_id: userId,
+    type,
+    tags: tags,
+    whats_great_about_it,
+    created_by: userName,
+    gem_id: createRandId()
   };
+
+  console.log("NEW GEM: ", newGem);
+  
 
   try {
     const result = await gems.insertOne(newGem);
     res.json(result).status(200);
-    
+
   } catch (error) {
     console.error('Failed to create gem:', error);
     res.status(500).json({ message: 'Internal server error' });
@@ -114,7 +133,7 @@ router.post('/create', async (req, res) => {
 // This will delete a Gem in the db using its _id-------------------------------------------------------------------
 router.delete('/delete/:id', async (req, res) => {
   const id = req.params.id;
-  
+
   try {
     const gems = await db.collection('gems');
     const result = await gems.deleteOne({ _id: new ObjectId(id) });
