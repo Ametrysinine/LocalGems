9594f9b0b0b4cdc78e3130f65b7514300785ca6c
Cynthia <cynthia-lam@outlook.com>
2024-08-08 22:28:08 -0400

    Change 4 rows of input to two columns

--- client/src/components/CreateGemForm.jsx
+++ client/src/components/CreateGemForm.jsx
@@ -71,71 +71,72 @@ const CreateGemForm = function(props) {
   return (
     <div className="creategemform-container">
       <form onSubmit={handleSubmit} className={`creategemform`}>
-        <div className="row-1">
-          <label>
-            Name of Gem:
-            <input type="text" name="name" value={formData.name} onChange={handleChange} required />
-          </label>
-          <label>
-            City:
-            <input type="text" name="city" value={formData.city} onChange={handleChange} required />
-          </label>
-          <label>Select type:</label>
-          <select
-            type="text" name="type" value={formData.type} onChange={handleChange} className="type" required>
-            <option value="">Select a type</option>
-            <option value="food">Food</option>
-            <option value="outdoors">Outdoors</option>
-            <option value="shopping">Shopping</option>
-            <option value="entertainment">Entertainment</option>
-            <option value="nightlife">Nightlife</option>
-            <option value="services">Services</option>
-          </select>
-        </div>
-        <br />
-        <div className="row-2">
-          <label>
-            Address:
-            <input type="text" name="address" value={formData.address} onChange={handleChange} required />
-          </label>
-          <label>
-            Latitude:
-            <input type="text" name="latitude" value={formData.latitude} onChange={handleChange} required />
-          </label>
-          <label>
-            Longitude:
-            <input type="text" name="longitude" value={formData.longitude} onChange={handleChange} required />
-          </label>
-        </div>
-        <br />
-        <div className="row-3">
-          <label>
-            Images (please separate URLs by comma):
-            <input type="text" name="images" value={formData.images} onChange={handleChange} required />
-          </label>
-          <label>
-            Tags (please separate by comma):
-            <input type="text" name="tags" value={formData.tags} onChange={handleChange} required />
-          </label>
-        </div>
-        <br />
-        <div className="row-4">
-          <label>
-            Description:
-            <textarea name="description" value={formData.description} onChange={handleChange} required placeholder="E.g. a hole in the wall pizza shop with giant slices" />
-          </label>
-          <label>
-            What's so great about it?
-            <textarea type="text" name="whats_great_about_it" value={formData.whats_great_about_it} onChange={handleChange} required placeholder="E.g. the 'OG Big Cheese' pizza" />
-          </label>
+        <div className="body">
+          <div className="column-1">
+            <label>
+              Name of Gem:
+              <input type="text" name="name" value={formData.name} onChange={handleChange} required />
+            </label>
+            <label>
+              City:
+              <input type="text" name="city" value={formData.city} onChange={handleChange} required />
+            </label>
+            <label>Select type:</label>
+            <select
+              type="text" name="type" value={formData.type} onChange={handleChange} className="type" required>
+              <option value="">Select a type</option>
+              <option value="food">Food</option>
+              <option value="outdoors">Outdoors</option>
+              <option value="shopping">Shopping</option>
+              <option value="entertainment">Entertainment</option>
+              <option value="nightlife">Nightlife</option>
+              <option value="services">Services</option>
+            </select>
+            <br />
 
+            <label>
+              Address:
+              <input type="text" name="address" value={formData.address} onChange={handleChange} required />
+            </label>
+            <label>
+              Latitude:
+              <input type="text" name="latitude" value={formData.latitude} onChange={handleChange} required />
+            </label>
+            <label>
+              Longitude:
+              <input type="text" name="longitude" value={formData.longitude} onChange={handleChange} required />
+            </label>
+
+          </div>
+          <br />
+          <div className="column-2">
+            <label>
+              Images (please separate URLs by comma):
+              <textarea type="text" name="images" value={formData.images} onChange={handleChange} required />
+            </label>
+            <label>
+              Tags (please separate by comma):
+              <textarea type="text" name="tags" value={formData.tags} onChange={handleChange} required />
+            </label>
+
+            <br />
+
+            <label>
+              Description:
+              <textarea name="description" value={formData.description} onChange={handleChange} required placeholder="E.g. a hole in the wall pizza shop with giant slices" />
+            </label>
+            <label>
+              What's so great about it?
+              <textarea type="text" name="whats_great_about_it" value={formData.whats_great_about_it} onChange={handleChange} required placeholder="E.g. the 'OG Big Cheese' pizza" />
+            </label>
+          </div>
         </div>
-        <div className="submit-button">
+
+        <div className="submit-section">
           <button type="submit">Submit</button>
         </div>
-
       </form>
-
+<br/>
     </div>
   );
 };
