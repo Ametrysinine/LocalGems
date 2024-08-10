10e935f0011c9f5d1f21d77401c56f479e6041f1
ChrisPytel <chris_pytel@hotmail.com>
2024-08-09 20:32:02 -0400

    update UnlockModal to correctly render and avoid event bubbling

--- client/src/components/UnlockModal.jsx
+++ client/src/components/UnlockModal.jsx
@@ -14,42 +14,82 @@ export default function UnlockModal({gemData, setUnlockModalVisibility}) {
     const gemImage = (type) => {
       switch (type) {
         case 'food':
-          return <img src="/assets/flaticons/gem_ruby.png" alt="Ruby - Food" className="gem-currency-image" />;
+          return <div className="gem-type">
+          <img src="/assets/flaticons/gem_ruby.png" alt="Ruby - Food" className="gem-currency-image" />
+          <h2 className="gem-type_ruby">-1</h2>
+        </div> 
         case 'entertainment':
-          return <img src="/assets/flaticons/gem_sapphire.png" alt="Sapphire - Entertainment" className="gem-currency-image" />;
+          return <div className="gem-type">
+          <img src="/assets/flaticons/gem_sapphire.png" alt="Sapphire - Entertainment" className="gem-currency-image" />
+          <h2 className="gem-type_sapphire">-1</h2>
+        </div>          
         case 'outdoors':
-          return <img src="/assets/flaticons/gem_emerald.png" alt="Emerald - Outdoor Activity" className="gem-currency-image" />;
+          return <div className="gem-type">
+          <img src="/assets/flaticons/gem_emerald.png" alt="Emerald - Outdoor Activity" className="gem-currency-image" />
+          <h2 className="gem-type_emerald">-1</h2>
+        </div>
         case 'shopping':
-          return <img src="/assets/flaticons/gem_topaz.png" alt="Topaz - Shopping" className="gem-currency-image" />;
+          return <div className="gem-type">
+          <img src="/assets/flaticons/gem_topaz.png" alt="Topaz - Shopping" className="gem-currency-image" />
+          <h2 className="gem-type_topaz">-1</h2>
+        </div>
         case 'nightlife':
-          return <img src="/assets/flaticons/gem_amethyst.png" alt="Amethyst - Nightlife" className="gem-currency-image" />;
+          return <div className="gem-type">
+          <img src="/assets/flaticons/gem_amethyst.png" alt="Amethyst - Nightlife" className="gem-currency-image" />
+          <h2 className="gem-type_amethyst">-1</h2>
+        </div>
         case 'services':
-          return <img src="/assets/flaticons/gem_citrine.png" alt="Citrine - Services" className="gem-currency-image" />;
+          return <div className="gem-type">
+          <img src="/assets/flaticons/gem_citrine.png" alt="Citrine - Services" className="gem-currency-image" />
+          <h2 className="gem-type_citrine">-1</h2>
+        </div>
       }
     };
   
+    //Very WET but i needed to get the name
+    const gemName = (type) => {
+      switch (type) {
+        case 'food':
+          return "Ruby";
+        case 'entertainment':
+          return "Sapphire";
+        case 'outdoors':
+          return "Emerald";
+        case 'shopping':
+          return "Topaz";
+        case 'nightlife':
+          return "Amethyst";
+        case 'services':
+          return "Citrine";
+      }
+    };
+
+    const checkCurrencyAmount = function() {            
+    	console.log(`checkCurrencyAmount - checking if we have a stored ${gemName(gemData?.type)} on server`);
+      
+      //post data to /currency here
+
+    };
+
   return (
     <>
     <div className="unlockModal-screen-space">
       <div className="unlockModal-container" onClick={() => console.log(`clicked unlock container`)}>  
 
-        <div className="unlockModal-container_are-you-sure"> 
+        <div className="unlock-step-1_are-you-sure"> 
 
-          <h3>
-            {`Are you sure you want to reveal this gem for  1 ${gemData?._id}`}
-          </h3>
-          
-          
-            <div  className="unlockModal-button">
-              <button className="success">
-                Unlock!
-              </button>
-              <button className="close" onClick={() => setUnlockModalVisibility(false)}>
-                Maybe Later
-              </button>
+          <div className="unlockModal-message">
+            <h3>
+              {`Are you sure you want to reveal this gem at the cost of a ${gemName(gemData?.type)}?`}
+            </h3>
+            <div className="unlockModal-gemstone">
+            {gemImage(gemData?.type)}
             </div>
-          <p>We've got gem id: {gemData?._id}</p> 
-          <p>Current user is: {userFromDB?.user_id}</p> 
+          </div>                    
+            <div  className="unlockModal-button">
+              <button className="success" onClick={checkCurrencyAmount}> Unlock! </button>
+              <button className="close" onClick={()=> setUnlockModalVisibility(false)}> Maybe Later </button>
+            </div>
         </div>
 
       </div>
