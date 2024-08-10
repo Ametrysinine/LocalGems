6f16f894d122ae4edff7605290a54534ec68455a
Cynthia <cynthia-lam@outlook.com>
2024-08-10 00:14:20 -0400

    Remove dead code

--- client/src/components/GemListItem.jsx
+++ client/src/components/GemListItem.jsx
@@ -246,25 +246,3 @@ const GemListItem = (props) => {
 };
 
 export default GemListItem;
-
-{/* 
-      <td className="p-4 align-middle [&:has([role=checkbox])]:pr-0">
-      <div className="flex gap-2">
-        <Link
-          className="inline-flex items-center justify-center whitespace-nowrap text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 border border-input bg-background hover:bg-slate-100 h-9 rounded-md px-3"
-          to={`/edit/${props.gem._id}`}
-        >
-          Edit
-        </Link>
-        <button
-          className="inline-flex items-center justify-center whitespace-nowrap text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 border border-input bg-background hover:bg-slate-100 hover:text-accent-foreground h-9 rounded-md px-3"
-          color="red"
-          type="button"
-          onClick={() => {
-            props.deleteGem(props.gem._id);
-          }}
-        >
-          Delete
-        </button>
-      </div>
-    </td> */}
